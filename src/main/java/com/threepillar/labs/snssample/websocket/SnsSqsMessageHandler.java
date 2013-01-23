21dae4fdd0e139f33d2d6869e28c1c7d4db0ce56
sayantam <sayantam@gmail.com>
2013-01-23 15:25:02 +0000

    Implemented SNS-SQS communication example

---
+++ src/main/java/com/threepillar/labs/snssample/websocket/SnsSqsMessageHandler.java
@@ -0,0 +1,72 @@
+package com.threepillar.labs.snssample.websocket;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.jetty.websocket.WebSocket.Connection;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.MessagingException;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageHandler;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.util.Assert;
+
+public class SnsSqsMessageHandler implements WebsocketHandler, MessageHandler,
+		ApplicationContextAware {
+
+	private final Log log = LogFactory.getLog(SnsSqsMessageHandler.class);
+
+	private ApplicationContext applicationContext;
+	private MessageChannel logSnsOutbound;
+	private DirectChannel logSqsInbound;
+	private Connection connection;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Override
+	public void handleMessage(Message<?> message) throws MessagingException {
+
+		Object payload = message.getPayload();
+		try {
+			connection.sendMessage(payload.toString());
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+	@Override
+	public void start() {
+		logSnsOutbound = applicationContext.getBean("logSnsOutbound",
+				MessageChannel.class);
+		Assert.notNull(logSnsOutbound, "'logSnsOutbound' must not be null");
+		logSqsInbound = applicationContext.getBean("logSqsInbound",
+				DirectChannel.class);
+		Assert.notNull(logSqsInbound, "'logSqsInbound' must not be null");
+		logSqsInbound.subscribe(this);
+	}
+
+	@Override
+	public void stop() {
+		logSqsInbound.unsubscribe(this);
+	}
+
+	@Override
+	public void postMessage(String data) {
+		logSnsOutbound.send(MessageBuilder.withPayload(data).build());
+	}
+
+}
--- src/main/webapp/log-sns-sqs.jsp
+++ src/main/webapp/log-sns-sqs.jsp
@@ -6,7 +6,7 @@
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>SNS Channel Log</title>
+<title>SNS-SQS Log</title>
 
 <script type='text/javascript'
   src='http://labs.3pillarglobal.com/wp-includes/js/jquery/jquery.js?ver=1.8.3'></script>
@@ -17,28 +17,22 @@
 <textarea rows="20" cols="50" id="clientMessage"></textarea>
 <button id="postToSNS">Post to SNS</button>
 
-<textarea rows="20" cols="50" id="serverMessageA" readonly="true"></textarea>
-<textarea rows="20" cols="50" id="serverMessageB" readonly="true"></textarea>
+<textarea rows="20" cols="50" id="serverMessage" readonly="true"></textarea>
 
 <script type="text/javascript">
 	jQuery(function($) {
-		var webSocketA = new WebSocket("ws://localhost:9090/snsChannelSubscriberA");
-		var webSocketB = new WebSocket("ws://localhost:9090/snsChannelSubscriberB");
-		webSocketA.onmessage = function(event) {
+		var webSocket = new WebSocket("ws://localhost:9090/snsSqsMessageHandler");
+		webSocket.onmessage = function(event) {
 			var messageText = event.data;
-			$("#serverMessageA").append(messageText + "\n");
-		};
-		webSocketB.onmessage = function(event) {
-			var messageText = event.data;
-			$("#serverMessageB").append(messageText + "\n");
+			$("#serverMessage").append(messageText + "\n");
 		};
 		
 		$("#postToSNS").click(function() {
 			var inputArea = $("#clientMessage");
 			var message = inputArea.attr("value");
 			if (message.length > 0) {
-				webSocketA.send(message);
-				inputArea.attr("value", "");
+                webSocket.send(message);
+                inputArea.attr("value", "");
 			}
 		});
 	});
